c626f431668781e30da134d2d5df1d8a7600f547
Ryuuramaru <rosumateisc@gmail.com>
2020-10-27 01:43:54 +0200

    Done
    Also added a ground texture, sky box and crosshair

---
+++ Assets/FinishedScripts/ForcePush.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ForcePush : MonoBehaviour
+{
+    public float force;
+    public float range;
+    //public Rigidbody rb;
+
+    RaycastHit hit;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            if (hit.collider.CompareTag("Pushable"))
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+        }
+
+        if (Input.GetMouseButtonDown(1) && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            if (hit.collider.CompareTag("Pushable"))
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1 * force * 0.6f, ForceMode.Impulse);
+        }
+
+        if (Input.GetButtonDown("Push") && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            if (hit.collider.CompareTag("Pushable"))
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * force * 0.4f, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/FinishedScripts/xhair.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xhair : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Texture2D crosshairImage;
+
+    void Start()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
