9b5a6e23e4485470dfe5b4cf424aac48f380fdc0
Ryuuramaru <rosumateisc@gmail.com>
2020-10-27 16:15:07 +0200

    WIP
    Made cubes transpareent and luminous
    Pull now works better
    Each force has different range and power
    Up and push now adds a slight spin to the cube to make it look more realistic

--- Assets/FinishedScripts/ForcePush.cs
+++ Assets/FinishedScripts/ForcePush.cs
@@ -7,6 +7,7 @@ public class ForcePush : MonoBehaviour
 {
     public float force;
     public float range;
+    public Transform childTransform;
     //public Rigidbody rb;
 
     RaycastHit hit;
@@ -18,22 +19,34 @@ public class ForcePush : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, transform.forward, out hit, range * 0.5f))
         {
             if (hit.collider.CompareTag("Pushable"))
+            {
                 hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * 50f);
+
+            }
         }
 
-        if (Input.GetMouseButtonDown(1) && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        if (Input.GetMouseButton(1) && Physics.Raycast(transform.position, transform.forward, out hit, range * 1.5f))
         {
-            if (hit.collider.CompareTag("Pushable"))
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1 * force * 0.6f, ForceMode.Impulse);
+            if (hit.collider.CompareTag("Pushable") && hit.distance >= 2.5f)
+            {
+                /*if (hit.distance <= 5f) hit.collider.gameObject.GetComponent<Rigidbody>().position = Vector3.Lerp(childTransform.transform.position, hit.collider.gameObject.GetComponent<Rigidbody>().position, 0.1f);
+                else*/ hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1 * force * 0.2f, ForceMode.Force);
+            }
+                
         }
 
-        if (Input.GetButtonDown("Push") && Physics.Raycast(transform.position, transform.forward, out hit, range))
+        if (Input.GetButtonDown("Push") && Physics.Raycast(transform.position, transform.forward, out hit, range * 0.5f))
         {
             if (hit.collider.CompareTag("Pushable"))
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * force * 0.4f, ForceMode.Impulse);
+            {
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * force * 0.5f, ForceMode.Impulse);
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * 20f);
+            }
         }
     }
 }
+
